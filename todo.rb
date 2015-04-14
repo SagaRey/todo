4b6bf800d5ba090d7152b9419ab8a61a2e0950bd
sagarey <saga.rey@outlook.com>
2015-04-14 16:20:08 +0800

    finish todolist

--- todo.rb
+++ todo.rb
@@ -19,27 +19,15 @@ get '/' do
   erb :index, layout: !request.xhr?
 end
 
-get '/todo' do
-  if session['user_id'].nil?
-    redirect to '/'
-  else
-    @user = User.find(session['user_id'])
-    @todolists = @user.todolists.where('status in (?)', ['active', 'completed'])
-    @count = @todolists.count
-    @left_items = @todolists.where('status = ?', 'active').count
-    erb :todo, layout: !request.xhr?
-  end
-end
-
 post '/signup' do
   name = params['name']
   password = params['password']
   repassword = params['repassword']
   if User.where('name = ?', name).count < 1 && password == repassword
     User.create!( name: name,
-                  password: password,
-                  updated_at: Time.now.utc,
-                  created_at: Time.now.utc )
+      password: password,
+      updated_at: Time.now.utc,
+      created_at: Time.now.utc )
     session['user_id'] = User.where('name = ?', name).first.id
     redirect to '/todo'
   else
@@ -61,4 +49,54 @@ end
 get '/logout' do
   session[:user_id] = nil
   redirect to '/'
+end
+
+get '/todo' do
+  redirect to '/' if session['user_id'].nil?
+  @user = User.find(session['user_id'])
+  @todolists = @user.todolists.where('status in (?)', ['actived', 'completed'])
+  @count = @todolists.count
+  @left_items = @todolists.where('status = ?', 'actived').count
+  erb :todo, layout: !request.xhr?
+end
+
+post '/todo' do
+  redirect to '/' if session['user_id'].nil?
+  content = params['content']
+  return if content.blank?
+  user = User.find(session['user_id'])
+  user.todolists.create!(
+    content: content,
+    status: 'actived',
+    updated_at: Time.now.utc,
+    created_at: Time.now.utc )
+  user.todolists.where('status in (?)', ['actived', 'completed']).count.to_s
+end
+
+put '/todo' do
+  redirect to '/' if session['user_id'].nil?
+  todolist_id = params['todolist_id']
+  return if todolist_id.blank?
+  todolist = Todolist.find(todolist_id)
+  if todolist.status == 'completed'
+    todolist.update_attribute(:status, 'actived')
+  elsif todolist.status == 'actived'
+    todolist.update_attribute(:status, 'completed')
+  end
+end
+
+delete '/todo' do
+  redirect to '/' if session['user_id'].nil?
+  todolist_id = params['todolist_id']
+  return if todolist_id.blank?
+  user = User.find(session['user_id'])
+  todolists = user.todolists.where('status = ?', 'completed')
+  if todolist_id == 'ids'
+    todolists.each do |todo|
+      todo.update_attribute(:status, 'deleted')
+    end
+  else
+    Todolist.find(todolist_id).update_attribute(:status, 'deleted')
+  end
+  user.todolists.where('status in (?)', ['actived', 'completed']).count.to_s
 end
